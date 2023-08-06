95ea02a43ad815c6182ccc173121e9b19441e784
Sergi Molina <sergimoli@gmail.com>
2023-08-06 19:04:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,156 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "Charger", quantity: 12, packed: false },
+// ];
+
+export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    //REmember we cannot do something like this(not allowed to mutate the state):
+    // setItems((items)=> items.push(item))
+    //the solution is to create a brand new array that contains the current items array + new item. We spread the current items and add another.
+    setItems((items) => [...items, item]);
+  }
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToogleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToogleItem={handleToogleItem}
+      />
+      <Stats items={items} />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴 Far away 💼</h1>;
+}
+function Form({ onAddItems }) {
+  // controlled elements: usestate + onchange
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  //we moved the following useState to App. This case is a technique called lift up state, because we need this state to be rendered in the packingList, but packinglist and Form are siblings components. Solution, move to the closed parent component.
+  // const [items, setItems] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return; //if no description do nothing!
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>what do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {/* here we add 20 options. so, instead of doing this:
+        <option value={1}>1</option>
+        <option value={1}>2</option>
+        <option value={1}>3</option> 
+        we do this trick:
+        */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        //it's sync with the state
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
+  );
+}
+function PackingList({ items, onDeleteItem, onToogleItem }) {
+  // const [sortBy, setSortBy] = useState("input");
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item
+            key={item.id}
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToogleItem={onToogleItem}
+          />
+        ))}
+      </ul>
+      {/* <div className="actions">
+        <select value={sortBy}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div> */}
+    </div>
+  );
+}
+
+function Item({ item, onDeleteItem, onToogleItem }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => {
+          onToogleItem(item.id);
+        }}
+      />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
+  );
+}
+
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding to your packing list 🚀</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = (numPacked / numItems) * 100;
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈"
+          : `💼 you have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
